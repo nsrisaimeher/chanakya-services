9cac3b011ddf89d8005c079067dfa5762a518e2f
playwillteach <nssmeher@gmail.com>
2017-04-08 13:00:42 -0500

    CS003 Connect to Mongo POC.
     Author:    playwillteach <nssmeher@gmail.com>
     Date:      Sat Apr 8 13:00:42 2017 -0500
     Changes to be committed:
    	modified:   .classpath
    	modified:   pom.xml
    	modified:   src/main/java/com/my_chanakya/chanakya_services/StudentResource.java
    	deleted:    target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    	deleted:    target/m2e-wtp/web-resources/META-INF/maven/com.my-chanakya/chanakya-services/pom.properties
    	deleted:    target/m2e-wtp/web-resources/META-INF/maven/com.my-chanakya/chanakya-services/pom.xml
    	modified:   target/maven-archiver/pom.properties

--- src/main/java/com/my_chanakya/chanakya_services/StudentResource.java
+++ src/main/java/com/my_chanakya/chanakya_services/StudentResource.java
@@ -3,10 +3,41 @@
  */
 package com.my_chanakya.chanakya_services;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 /**
  * @author nssmeher
  *
  */
-public class StudentResource {
 
+@Path("/hello")
+public class StudentResource {
+	
+	
+
+		@GET
+		@Path("/{param}")
+		public Response getMsg(@PathParam("param") String msg) {
+			
+			MongoClient mongo = new MongoClient();
+			MongoDatabase db = mongo.getDatabase("chanakya");
+			db.createCollection("problemCollection");
+//			MongoCollection<Document> collection = 
+
+			String output = "Jersey say : " + msg;
+
+			return Response.status(200).entity(output).build();
+
+		}
+
+	
 }
